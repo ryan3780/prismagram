cd4fe346d9e85d2d20a8a0fc15e17f3bc87b8123
ryan3780 <guemhong3780@gmail.com>
2019-05-15 16:43:05 +0900

    1st send

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # prismagram
+
 instagram clone with Express + Prisma +React And React-Navtive
+
+## User Stories
+
+- [x] create account
+- [ ] request secret
+- [ ] confrim secret (login)
+- [ ] like / unlike a photo
+- [ ] comment on a photo
+- [ ] search by user
+- [ ] search by location
+- [ ] see user profile
+- [ ] follow / unfollow user
+- [ ] see the full photo
+- [ ] edit my profile
+- [ ] upload photos
+- [ ] edit the photo (delete)
+- [ ] see the feed
---
+++ src/utils.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "guemhong3780prisma.com",
+    to: address,
+    subject: "🔒Login Secret for prismagram🔒",
+    html: `Hello! your login secret is ${secret}.</br> copy paste on the app/web log in`
+  };
+  return sendMail(email);
+};
